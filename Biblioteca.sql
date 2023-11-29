214d6fa38d9dc8f95f266eb1d2c9bb59ade9d9f9
gabrieljmr <126875509+gabrieljmr@users.noreply.github.com>
2023-11-28 22:48:23 -0300

    Add files via upload

--- Biblioteca.sql
+++ Biblioteca.sql
@@ -13,6 +13,8 @@ create table Livros(
 )
 insert into livros (id_livro, titulo, autor, ano_publicacao, genero, quantidade_disponivel)
 values(1, 'O código Da Vinci','Dan Brown', 2003, 'romance', 20)
+insert into livros (id_livro, titulo, autor, ano_publicacao, genero, quantidade_disponivel)
+values(2, 'Harry Potter e a Pedra Filosofal','J K Rowling', 1997, 'Fantasia', 50)
 create table Usuarios( 
     id_usuario int auto_increment primary key,
     nome varchar(50) ,
@@ -22,7 +24,8 @@ create table Usuarios(
 )
 insert into usuarios (id_usuario, nome, cpf, email, telefone)
 values(1, 'Manoel',12345678910, 'manoel@gmail.com', 912345678)
-
+insert into usuarios (id_usuario, nome, cpf, email, telefone)
+values(2, 'Claudia',12345678980, 'claudia@gmail.com', 942345678)
 show tables
 describe livros
 describe usuarios
